ac11e43e140b499fe12d6f56450a04ac27214e8a
Jamison Burton <jamison@burtontalk.com>
2022-02-11 08:49:54 -0700

    Add project files.

---
+++ RollTheDice/RollTheDice.vb
@@ -0,0 +1,29 @@
+﻿'Jamison Burton
+'RCET 0265
+'Spring 2022
+'PUT URL HERE!!!
+Module RollTheDice
+
+    Sub Main()
+        Dim firstRoll As Integer
+        Dim secondRoll As Integer
+        Dim Roll(11) As Integer
+        Dim Result(11) As Integer
+
+        Console.WriteLine("Roll of the Dice")
+        Console.WriteLine((StrDup(70, "-")))
+
+        For i = 0 To 99
+            firstRoll = RollDice()
+            secondRoll = RollDice()
+        Next
+
+        Console.ReadLine()
+    End Sub
+    Function RollDice() As Integer
+        Randomize()
+        Dim value As Integer = CInt(Int((6 * Rnd()) + 1))
+        Return value
+    End Function
+
+End Module
