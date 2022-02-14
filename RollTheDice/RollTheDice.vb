a74ef8f07de4957dc2dd57da6eb1a3595c30b9ab
Jamison Burton <jamison@burtontalk.com>
2022-02-14 08:20:30 -0700

    Added in display of result array

--- RollTheDice/RollTheDice.vb
+++ RollTheDice/RollTheDice.vb
@@ -7,16 +7,30 @@ Module RollTheDice
     Sub Main()
         Dim firstRoll As Integer
         Dim secondRoll As Integer
-        Dim Roll(12) As Integer
         Dim Result(12) As Integer
 
-        Console.WriteLine("Roll of the Dice")
-        Console.WriteLine((StrDup(70, "-")))
+        'Writes out title
+        Console.WriteLine(CStr("Roll of the Dice").PadLeft(45))
+        Console.WriteLine((StrDup(90, "-")))
 
-        For i = 0 To 99
+        'perform calcutations using Randomizer with dice values and adds to Result array
+        For i = 0 To 999
             firstRoll = RollDice()
             secondRoll = RollDice()
             Result(firstRoll + secondRoll) += 1
+        Next
+
+        'displays array columns
+        For i = 2 To UBound(Result)
+            Console.Write(CStr(i).PadLeft(6) & " |")
+        Next
+
+        Console.WriteLine("")
+        Console.WriteLine((StrDup(90, "-")))
+
+        'displays array results
+        For i = 2 To UBound(Result)
+            Console.Write(CStr(Result(i)).PadLeft(6) & " |")
         Next
 
         Console.ReadLine()
@@ -24,7 +38,7 @@ Module RollTheDice
     Function RollDice() As Integer
         Randomize()
         Dim value As Integer = CInt(Int((6 * Rnd()) + 1))
-        Return value
+        Return value 'Outputs a random value between 1 and 6
     End Function
 
 End Module
